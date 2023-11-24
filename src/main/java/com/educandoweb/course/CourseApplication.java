a353e1f835900cbe417942ccd2f48081d338ced3
Pedro Paulo <012pedropaulo@gmail.com>
2023-11-23 21:40:35 -0300

    test

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -8,7 +8,7 @@ public class CourseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
-		System.out.println("Works!");
+		System.out.println("This Works!");
 	}
 
 }
